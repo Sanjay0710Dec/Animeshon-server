6c317a3d20cb01149495114dbdeb4b468b91db55
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-12-03 11:22:36 +0530

    added feedback-suggestion-model

---
+++ prisma/migrations/20241202142722_added_model_for_user_feeback_and_suggestion/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "UserFeedBack" (
+    "id" TEXT NOT NULL,
+    "feedback" TEXT NOT NULL,
+    "suggestion" TEXT,
+
+    CONSTRAINT "UserFeedBack_pkey" PRIMARY KEY ("id")
+);
